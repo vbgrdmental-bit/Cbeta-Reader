276e7681273e2b22a5df8d7a0f12fbd2ef0b2d64
vbgrdmental-bit <vbgrdmental@gmail.com>
2026-08-06 21:29:32 +0800

    異體字擴充

--- src/reader/components/ReaderView.tsx
+++ src/reader/components/ReaderView.tsx
@@ -1287,7 +1287,7 @@ export function ReaderView({
       case 'wenkai':
       case 'yuanti':
       case 'fangsong':
-        return '"TW-Kai-98", "MOE-EduKai", "TW-Kai", "DFKai-SB", "標楷體", "BiauKai", "Microsoft JhengHei", "PingFang TC", "CBETASupplement", serif';
+        return '"TW-Kai-98", "MOE-EduKai", "TW-Kai", "DFKai-SB", "標楷體", "BiauKai", "TW-Kai Ext-B", "TW-Kai Plus", "CBETASupplement", serif';
       case 'default':
       default:
         return 'var(--font-serif)';
