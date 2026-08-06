95c5fb9e7506e4cc455d0e523074a377e8d1fcd3
vbgrdmental-bit <vbgrdmental@gmail.com>
2026-08-06 23:40:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
       '/api-cbeta': {
         target: 'https://cbdata.dila.edu.tw',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-cbeta/, '')
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api-cbeta/, ''),
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+          'Accept': 'application/json, text/plain, */*',
+          'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7'
+        }
       }
     }
   }
